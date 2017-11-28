fa3b2dc0a34229b1f50fc2fc570d070399a5bd07
mu <hmu332233@gmail.com>
2017-11-29 01:05:52 +0900

    [Add] Counter project - components

---
+++ basic/template.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  
+};
+
+const defaultProps = {
+  
+};
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>App</div>
+    );
+  }
+}
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
+
+export default App;
---
+++ project2/redux-example/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Counter from './Counter';
+
+const propTypes = {
+  
+};
+
+const defaultProps = {
+  
+};
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <Counter/>
+    );
+  }
+}
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
+
+export default App;
