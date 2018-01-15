828611b4b8f2fbeec4e8478353d316c9c173d970
mu <hmu332233@gmail.com>
2018-01-16 02:17:32 +0900

    [Modify] 2. state가 어디에 있어야할지 찾기

--- docs/ThinkingInReact/src/index.js
+++ docs/ThinkingInReact/src/index.js
@@ -39,10 +39,21 @@ class ProductRow extends React.Component {
 
 class ProductTable extends React.Component {
   render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+    
     const rows = [];
     let lastCategory = null;
     
     this.props.products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      
+      
       if (product.category !== lastCategory) {
         rows.push(
           <ProductCategoryRow
@@ -88,11 +99,25 @@ class SearchBar extends React.Component {
 }
 
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    }
+  }
   render() {
     return (
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar 
+      		filterText={this.state.filterText}
+      		inStockOnly={this.state.inStockOnly}
+      	/>
+        <ProductTable 
+      		products={this.props.products} 
+      		filterText={this.state.filterText}
+      		inStockOnly={this.state.inStockOnly}
+      	/>
       </div>
     );
   }
