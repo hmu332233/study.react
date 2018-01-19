0dd6ff5c84803595eb1b381d645598cae74bc9f6
mu <hmu332233@gmail.com>
2018-01-20 03:04:37 +0900

    [Add] api account signin

--- project3/server/routes/account.js
+++ project3/server/routes/account.js
@@ -56,7 +56,55 @@ router.post('/signup', function (req, res) {
   });
 });
 
+
+/*
+    ACCOUNT SIGNIN: POST /api/account/signin
+    BODY SAMPLE: { "username": "test", "password": "test" }
+    ERROR CODES:
+        1: LOGIN FAILED
+*/
 router.post('/signin', function (req, res) {
+  
+  if (typeof req.body.password !== 'string') {
+    return res.status(401).json({
+      error: 'LOGIN FAILED',
+      code: 1
+    });
+  }
+  
+  // FIND THE USER BY USERNAME
+  Account.findOne({ username: req.body.username }, function (err, accout) {
+    if (err) throw err;
+    
+    // CHECK ACCOUNT EXISTANCY
+    if (!account) {
+      return res.status(401).json({
+        error: 'LOGIN FAILED',
+        code: 1
+      });
+    }
+    
+    // CHECK WHETHER THE PASSWORD IS VALID
+    if (!account.validateHash(req.body.password)) {
+      return res.status(401).json({
+        error: 'LOGIN FAILED',
+        code: 1
+      });
+    }
+    
+    // ALTER SESSION
+    let session = req.session;
+    session.loginInfo = {
+      _id: account._id,
+      username: account.username
+    };
+    
+    // RETURN SUCCESS
+    return res.json({
+      success: true
+    });
+  });
+  
   res.json({ success: true });
 });
 
