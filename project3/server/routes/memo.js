86a5a63effbec70d4ebe5d19876e28b0d49b376d
mu <hmu332233@gmail.com>
2018-01-21 00:45:12 +0900

    [Add] api memo skeleton

---
+++ project3/server/routes/memo.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import Memo from '../models/memo';
+import mongoose from 'mongoose';
+
+const router = express.Router();
+
+// WRITE MEMO
+router.post('/', (req, res) => {
+
+});
+
+// MODIFY MEMO
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE MEMO
+router.delete('/:id', (req, res) => {
+
+});
+
+// GET MEMO LIST
+router.get('/', (req, res) => {
+
+});
+
+export default router;
