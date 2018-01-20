416c8c4528881aeb60319c5b811727564b647385
mu <hmu332233@gmail.com>
2018-01-21 00:50:30 +0900

    [Add] api memo write

--- project3/server/routes/memo.js
+++ project3/server/routes/memo.js
@@ -4,9 +4,41 @@ import mongoose from 'mongoose';
 
 const router = express.Router();
 
-// WRITE MEMO
+/* 
+    WRITE MEMO: POST /api/memo
+    BODY SAMPLE: { contents: "sample "}
+    ERROR CODES
+        1: NOT LOGGED IN
+        2: EMPTY CONTENTS
+*/
 router.post('/', (req, res) => {
-
+  // CHECK LOGIN STATUS
+  if (typeof req.session.loginInfo === 'undefined') {
+    return res.status(403).json({
+      error: "NOT LOGGED IN",
+      code: 1
+    });
+  }
+  
+  // CHECK CONTENTS VALID
+  if (typeof req.body.contents !== 'string' || req.body.contents === "") {
+    return res.status(400).json({
+      error: "EMPTY CONTENTS",
+      code: 2
+    });
+  }
+  
+  // CREATE NEW MEMO
+  let memo = new Memo({
+    writer: req.session.loginInfo.username,
+    contents: req.body.contents
+  });
+  
+  // SAVE IN DATABASE
+  memo.save(function (err) {
+    if (err) throw err;
+    return res.json({ success: true });
+  });
 });
 
 // MODIFY MEMO
