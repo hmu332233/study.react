2ae7a2e3fefd1f7c189a026bbf85cb653286a0a0
minung.han <hmu332233@gmail.com>
2021-05-20 01:41:06 +0900

    createAsyncHandler 예제 추가

--- vlpt/basic/src/app/Api/UsersContext.js
+++ vlpt/basic/src/app/Api/UsersContext.js
@@ -2,76 +2,26 @@ import React, { createContext, useReducer, useContext } from 'react';
 import axios from 'axios';
 
 import * as api from './api';
-import { createAsyncDispatcher } from './asyncActionUtils';
+import { createAsyncDispatcher, createAsyncHandler, initialAsyncState } from './asyncActionUtils';
 
-// UsersContext 에서 사용 할 기본 상태
 const initialState = {
-  users: {
-    loading: false,
-    data: [],
-    error: null
-  },
-  user: {
-    loading: false,
-    data: [],
-    error: null
-  }
+  users: initialAsyncState,
+  user: initialAsyncState
 };
 
-// 로딩중일 때 바뀔 상태 객체
-const loadingState = {
-  loading: true,
-  data: [],
-  error: null
-};
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
 
-// 성공했을 때의 상태 만들어주는 함수
-const success = data => ({
-  loading: false,
-  data,
-  error: null
-});
-
-// 실패했을 때의 상태 만들어주는 함수
-const error = error => ({
-  loading: false,
-  data: [],
-  error: error
-});
-
-// 위에서 만든 객체 / 유틸 함수들을 사용하여 리듀서 작성
 function usersReducer(state, action) {
   switch (action.type) {
     case 'GET_USERS':
-      return {
-        ...state,
-        users: loadingState
-      };
     case 'GET_USERS_SUCCESS':
-      return {
-        ...state,
-        users: success(action.data)
-      };
     case 'GET_USERS_ERROR':
-      return {
-        ...state,
-        users: error(action.error)
-      };
+      return usersHandler(state, action);
     case 'GET_USER':
-      return {
-        ...state,
-        user: loadingState
-      };
     case 'GET_USER_SUCCESS':
-      return {
-        ...state,
-        user: success(action.data)
-      };
     case 'GET_USER_ERROR':
-      return {
-        ...state,
-        user: error(action.error)
-      };
+      return userHandler(state, action);
     default:
       return state;
   }
--- vlpt/basic/src/app/Api/asyncActionUtils.js
+++ vlpt/basic/src/app/Api/asyncActionUtils.js
@@ -24,3 +24,62 @@ export function createAsyncDispatcher(type, promiseFn) {
   }
   return actionHandler; // 만든 함수를 반환합니다.
 }
+
+export const initialAsyncState = {
+  loading: false,
+  data: [],
+  error: null
+};
+
+// 로딩중일 때 바뀔 상태 객체
+const loadingState = {
+  loading: true,
+  data: [],
+  error: null
+};
+
+// 성공했을 때의 상태 만들어주는 함수
+const success = data => ({
+  loading: false,
+  data,
+  error: null
+});
+
+// 실패했을 때의 상태 만들어주는 함수
+const error = error => ({
+  loading: false,
+  data: [],
+  error: error
+});
+
+export function createAsyncHandler(type, key) {
+  // 성공, 실패에 대한 액션 타입 문자열을 준비합니다.
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  // 함수를 새로 만들어서
+  function handler(state, action) {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data)
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error)
+        };
+      default:
+        return state;
+    }
+  }
+
+  // 반환합니다
+  return handler;
+}
